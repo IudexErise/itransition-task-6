3f405d3ea05e18ade0f812044722e8b63cf89334
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2022-11-25 10:51:21 +0600

    heroku 4

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.use(express.json());
 
